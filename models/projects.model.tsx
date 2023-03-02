4879e6bc64a9f48dea33442178d9ed21ec4ed262
VirtualPirate <artaza13020artaza@gmail.com>
2023-03-02 20:35:39 +0530

    Added mongodb to the projects

--- models/projects.model.tsx
+++ models/projects.model.tsx
@@ -38,7 +38,7 @@ const projects: Array<ProjectType> = [
     name: "OpenGraph API",
     codeLink: "https://github.com/VirtualPirate/OpenGraph-API",
     deployLink: "https://opengraph.cyclic.app/",
-    createdUsing: ["javascript"],
+    createdUsing: ["javascript", "mongodb"],
   },
 
   {
