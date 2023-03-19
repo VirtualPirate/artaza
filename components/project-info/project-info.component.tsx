4a9f70f40d7b6812b9cb6db65394caec72fc852c
VirtualPirate <artaza13020artaza@gmail.com>
2023-03-19 16:55:07 +0530

    FIxed more issues with AOS

--- components/project-info/project-info.component.tsx
+++ components/project-info/project-info.component.tsx
@@ -30,41 +30,19 @@ export default function ProjectInfo({
   }, []);
 
   return (
-    <div className={styles.info}>
-      <img
-        className={styles.image}
-        src={image}
-        data-aos="fade-right"
-        data-aos-duration="1000"
-        data-aos-delay="100"
-      />
+    <div
+      className={styles.info}
+      data-aos="zoom-in"
+      data-aos-duration="1000"
+      data-aos-delay="100"
+    >
+      <img className={styles.image} src={image} />
       <div className={styles.project_info}>
-        <div
-          className={styles.title}
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          data-aos-delay="100"
-        >
-          {name}
-        </div>
-        <p
-          className={styles.description}
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          data-aos-delay="200"
-        >
-          {description}
-        </p>
+        <div className={styles.title}>{name}</div>
+        <p className={styles.description}>{description}</p>
         <div className={styles.buttons}>
           <Link href={visitLink} target="blank_">
-            <button
-              className={styles.visit_button}
-              data-aos="fade-left"
-              data-aos-duration="1000"
-              data-aos-delay="300"
-            >
-              {visitButtonText}
-            </button>
+            <button className={styles.visit_button}>{visitButtonText}</button>
           </Link>
 
           {codeLink !== "" ? (
